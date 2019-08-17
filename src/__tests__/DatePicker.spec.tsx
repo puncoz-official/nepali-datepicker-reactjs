19bd7d4d9371d5d74c7b4ef17110014447fa3658
Puncoz Nepal <puncoz138+git@gmail.com>
2019-08-17 19:45:30 +0545

    v1.0.1

--- src/__tests__/DatePicker.spec.tsx
+++ src/__tests__/DatePicker.spec.tsx
@@ -3,7 +3,7 @@ import { create as rendererCreate } from "react-test-renderer"
 import DatePicker from ".."
 
 test("Component should show 'red' text 'Hello World!'", () => {
-    const component = rendererCreate(<DatePicker name="World"/>)
+    const component = rendererCreate(<DatePicker name="World" />)
     const testInstance = component.root
 
     expect(testInstance.findByType(DatePicker).props.name).toBe("World")
